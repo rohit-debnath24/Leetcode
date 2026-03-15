949b56015614392367fec213630f5f84c70631dd
ROHIT KUMAR DEBNATH <rohit.kumardebnath.cse28@heritageit.edu.in>
2026-03-15 22:08:39 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (96.36%) - LeetSync

---
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        if (m == 0) {
+            nums1[0] = nums2[0];
+        }
+
+        int p1 = m - 1;
+        int p2 = n - 1;
+        int p3 = m + n - 1;
+        while (p1 >= 0 && p2 >= 0) {
+
+            if (nums2[p2] >= nums1[p1]) {
+                nums1[p3] = nums2[p2];
+
+                p2--;
+            } else {
+                nums1[p3] = nums1[p1];
+
+                p1--;
+            }
+            p3--;
+        }
+
+        while(p2>=0){
+             nums1[p3] = nums2[p2];
+
+                p2--;
+                p3--;
+        }
+    }
+};
