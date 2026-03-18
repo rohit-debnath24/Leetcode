4afecd6064b84e0475a45baff766ac22f7bb1b07
ROHIT KUMAR DEBNATH <rohit.kumardebnath.cse28@heritageit.edu.in>
2026-03-18 11:55:46 +0530

    Time: 0 ms (100.00%) | Memory: 17 MB (71.49%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
@@ -6,44 +6,45 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
-     
-        //go insde the tree and check for the node 
-        //hold the root node inside the stack
-        // following the root go inside the tree 
-        //take the root follow the childs 
-        //store the childs n the stack 
-        //then store the root in que 
 
         vector<vector<int>> ans;
         if(root==NULL) return ans;
+        vector<int> level;
         queue<TreeNode*> q;
         q.push(root);
+        q.push(NULL);
+        TreeNode* temp;
 
-        while(!q.empty()){
-            int size=q.size();
-        
-            vector<int>level;
-            for(int i=0;i<size;i++){
-                TreeNode*node=q.front();
-                q.pop();
-                if(node->left!=NULL) q.push(node->left);
-                if(node->right!=NULL) q.push(node->right);
 
-                level.push_back(node->val);    
+        while (!q.empty()) {
+            temp = q.front();
+            q.pop();
+
+            if(temp==NULL){
+                   ans.push_back(level);
+                    level.clear();
+
+                if(!q.empty()){
+                    q.push(NULL);
+                }
+
+            }else{
+                level.push_back(temp->val);
+                if (temp->left)
+                    q.push(temp->left);
+                if (temp->right)
+                    q.push(temp->right);
             }
-            ans.push_back(level);
-        }    
+        }
+
+
         return ans;
-        
-
-
-
-
     }
 };
