710c74ccc27bc436286325dadb5e8e01d8bd10d1
ROHIT KUMAR DEBNATH <rohit.kumardebnath.cse28@heritageit.edu.in>
2026-04-01 00:06:03 +0530

    Time: 0 ms (100.00%) | Memory: 18.9 MB (76.98%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -1,27 +1,34 @@
-#include <queue>
-#include <algorithm>
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    int maxDepth(TreeNode* root) {
-        if (!root) return 0;
+    
+    int calMax(TreeNode* root){
+        if(root==NULL)
+                return 0;
+        //  if(root->left ==NULL )
+        //         return 0;
+        //         if(root->right==NULL)
+        //         return 0;
 
-        std::queue<TreeNode*> q;
-        q.push(root);
-        int depth = 0;
-
-        while (!q.empty()) {
-            int levelSize = q.size();
-            depth++;
-
-            for (int i = 0; i < levelSize; ++i) {
-                TreeNode* curr = q.front();
-                q.pop();
-
-                if (curr->left) q.push(curr->left);
-                if (curr->right) q.push(curr->right);
-            }
+                int l=calMax(root->left);
+                int r=calMax(root->right);
+            //    int sum=l+r;
+                // res=max(res,sum);
+           return 1+max(l,r); 
         }
-        return depth;
+    int maxDepth(TreeNode* root) {
+        int res=0;
+        
+     return calMax(root); 
     }
 };
