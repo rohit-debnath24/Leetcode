d38ff26267aa78f1de75027dfbcc0013a230343b
ROHIT KUMAR DEBNATH <rohit.kumardebnath.cse28@heritageit.edu.in>
2026-05-25 06:18:38 +0530

    Time: 3 ms (37.94%) | Memory: 21.2 MB (8.01%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,40 +1,45 @@
 class Solution {
 private:
-        void dfs(int node,vector<vector<int>>&adj,vector<int>&visited)
-        {
-            visited[node]=1;
-            for(int i=0;i<adj[node].size();i++)
-            {
-                int neighb=adj[node][i];
-                if(visited[neighb]==0)
-                    dfs(neighb,adj,visited);
+    int dfs_base(int node, vector<int>& ls, vector<vector<int>>& adj,
+                 vector<int>& vis) {
+        vis[node] = 1;
+        ls.push_back(node);
+
+        for (auto it : adj[node]) {
+
+            if (!vis[it]) {
+                dfs_base(it, ls, adj, vis);
             }
-            return;
         }
+        return count;
+    }
+
 public:
+    int count = 0;
     int findCircleNum(vector<vector<int>>& isConnected) {
-        int n1=isConnected.size();
-        vector<vector<int>>adj(n1);
-        for(int i=0;i<isConnected.size();i++)
-        {
-            for(int j=0;j<isConnected[0].size();j++)
-            {
-                if(isConnected[i][j]==1 && i!=j)
-                {
+
+        int n1 = isConnected.size();
+        vector<vector<int>> adj(n1);
+        for (int i = 0; i < isConnected.size(); i++) {
+            for (int j = 0; j < isConnected[0].size(); j++) {
+                if (isConnected[i][j] == 1 && i != j) {
                     adj[i].push_back(j);
                     adj[j].push_back(i);
                 }
             }
         }
-        int count=0;
-        int n=adj.size();
-        vector<int>visited(n,0);
-        for(int i=0;i<n;i++)
-        {
-            if(visited[i]==0)
-            {
-                dfs(i,adj,visited);
+
+        int count = 0;
+
+        // Code here
+        vector<int> ls;
+        int n = adj.size();
+        vector<int> vis(n, 0);
+        for (int i = 0; i < n; i++) {
+            if (vis[i] == 0) {
+
                 count++;
+               dfs_base(i, ls, adj, vis);
             }
         }
         return count;
