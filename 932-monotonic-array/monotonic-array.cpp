65d4a1d8d54e012f8d817319c05dd36458690e63
ROHIT KUMAR DEBNATH <rohit.kumardebnath.cse28@heritageit.edu.in>
2026-07-03 01:52:12 +0530

    Time: 3 ms (31.07%) | Memory: 100.5 MB (33.54%) - LeetSync

--- 932-monotonic-array/monotonic-array.cpp
+++ 932-monotonic-array/monotonic-array.cpp
@@ -1,45 +1,21 @@
 class Solution {
 public:
     bool isMonotonic(vector<int>& nums) {
-            int n=nums.size();
-            bool status;
-            bool statusf;
-            bool statusc;
-            int tickf=1;
-            int tickc=1;
-            // bool status;
-
-            if(n==0 || n==1){
-                return true;
-            }
+        bool inc=false;
+        bool dec=false;
+        int n=nums.size();
         for(int i=0;i<n-1;i++){
-            if(nums[i]>=nums[i+1] && tickf==1 ){
-                statusf=true;
-                tickf=1;
+            if(nums[i]<nums[i+1]){
+                inc=true;
             }
-            else{
-                statusf=false;
-                tickf=0;
+            if(nums[i]>nums[i+1]){
+                dec=true;
             }
         }
-        for(int i=0;i<n-1;i++){
-            if(nums[i]<=nums[i+1] && tickc==1 ){
-                statusc=true;
-                tickc=1;
-            }
-            else{
-                statusc=false;
-                tickc=0;
-            }
+        if(inc==true&&dec==true){
+            return false;
         }
 
-
-        if(statusf==false && statusc==false){
-            status=false;
-        }else{
-            status=true;
-        }
-
-        return status;
+        return true;
     }
 };
