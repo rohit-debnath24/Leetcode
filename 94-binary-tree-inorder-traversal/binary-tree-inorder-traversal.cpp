23a0e7c2a240b3570ebef9524afa8b07cb8f6f56
ROHIT KUMAR DEBNATH <rohit.kumardebnath.cse28@heritageit.edu.in>
2026-07-04 23:53:51 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (66.01%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -11,37 +11,17 @@
  */
 class Solution {
 public:
-    vector<int> inorderTraversal(TreeNode* root) {
-        vector<int> ans;
-
-        TreeNode* curr=root;
-        while(curr!=NULL){
-            if(curr->left==NULL){
-                ans.push_back(curr->val);
-                curr=curr->right;
-
-            }else{
-                TreeNode*IP=curr->left;
-                while(IP->right!=NULL && IP->right!=curr){
-                    IP=IP->right;
-                    
-                }
-
-                if(IP->right==NULL){
-                    IP->right=curr;
-                    curr=curr->left;
-                }else{
-                    IP->right=NULL;
-                    ans.push_back(curr->val);
-                    curr=curr->right;
-
-                }
-            }
+    void inorder(TreeNode* root,vector<int>& ans){
+        if(root==NULL){
+            return;
         }
-       
-
-
-
+        inorder(root->left,ans);
+        ans.push_back(root->val);
+        inorder(root->right,ans);
+    }
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector <int> ans;
+        inorder(root,ans);
         return ans;
     }
 };
