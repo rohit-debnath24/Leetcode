88e40adbaf3ab8c5d03e4b57f809441091e25dc7
ROHIT KUMAR DEBNATH <rohit.kumardebnath.cse28@heritageit.edu.in>
2026-07-06 00:04:52 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (16.02%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
@@ -1,58 +1,27 @@
-#include <vector>
-#include <algorithm>
-#include <stack>
-
-using namespace std;
-
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
- * int val;
- * TreeNode *left;
- * TreeNode *right;
- * TreeNode() : val(0), left(nullptr), right(nullptr) {}
- * TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- * TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
 class Solution {
 public:
-    // Approach 1: Recursive (Cleanest)
-    void help(TreeNode* root, vector<int>& res) {
-        if (!root) return;
-        help(root->left, res);
-        help(root->right, res);
-        res.push_back(root->val);
-    }
-
-    vector<int> postorderTraversalRecursive(TreeNode* root) {
-        vector<int> res;
-        help(root, res);
-        return res;
-    }
-
-    // Approach 2: Iterative (Using a Stack)
-    vector<int> postorderTraversal(TreeNode* root) {
-        vector<int> res;
-        if (!root) return res;
-
-        stack<TreeNode*> s;
-        s.push(root);
-
-        while (!s.empty()) {
-            TreeNode* curr = s.top();
-            s.pop();
-            res.push_back(curr->val);
-
-            // Push left then right so that right is processed first 
-            // (creating Root -> Right -> Left order)
-            if (curr->left) s.push(curr->left);
-            if (curr->right) s.push(curr->right);
+    void post(TreeNode* root,vector<int>& ans){
+        if(!root){
+            return ;
         }
-
-        // Reverse the result to get Left -> Right -> Root
-        reverse(res.begin(), res.end());
-        return res;
+        post(root->left,ans);
+        post(root->right,ans);
+        ans.push_back(root->val);
+    }
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        post(root,ans);
+        return ans;
     }
 };
