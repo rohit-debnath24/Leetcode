496dab93fd774d69ce1f2e7464eab1acc2f42fb9
ROHIT KUMAR DEBNATH <rohit.kumardebnath.cse28@heritageit.edu.in>
2026-07-07 23:09:07 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (47.67%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -1,11 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    bool isSameTree(TreeNode* p, TreeNode* q) {
-        if (!p || !q) {
-            return p == q;
+    void check(TreeNode* p, TreeNode* q,bool &x){
+        if(!p||!q){
+            return;
         }
-        return (p->val == q->val) && 
-               isSameTree(p->left, q->left) && 
-               isSameTree(p->right, q->right);
+        if(p->val!=q->val){
+            x= false;
+        }
+        if(p->left!=NULL && q->left!=NULL ){
+        check(p->left,q->left,x);
+        }else if (p->left!=NULL && q->left==NULL){
+            x=false;
+        }else if(p->left==NULL && q->left!=NULL){
+            x=false;
+        }
+
+        if(p->right!=NULL && q->right!=NULL){
+        check(p->right,q->right,x);
+        }else if(p->right!=NULL && q->right==NULL){
+            x=false;
+        }else if(p->right==NULL && q->right!=NULL){
+            x=false;
+        }
+
+        
+    }
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if((!p && q) ||(p && !q) ){
+            return false;
+        }
+        bool x=true;
+        check(p,q,x);
+        return x;
     }
 };
