673b16c14198fc9197356b083d0f710ea6514df8
ROHIT KUMAR DEBNATH <rohit.kumardebnath.cse28@heritageit.edu.in>
2026-09-08 12:04:26 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (33.65%) - LeetSync

---
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        vector<int> ans;
+        int n=numbers.size();
+        int i=0;
+        int j=n-1;
+        while(i<j){
+            if(numbers[i]+numbers[j]<target){
+                i++;
+            }else if(numbers[i]+numbers[j]>target){
+                j--;
+            }else{
+                ans.push_back(i+1);
+                ans.push_back(j+1);
+                return ans;
+            }
+        }
+        return ans;
+    }
+};
