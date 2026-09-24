e8e4be5fb233c832fa5dd74690275bc3fa3bd325
ROHIT KUMAR DEBNATH <rohit.kumardebnath.cse28@heritageit.edu.in>
2026-09-24 12:50:00 +0530

    Time: 0 ms (100.00%) | Memory: 30.8 MB (98.04%) - LeetSync

---
+++ 3869-smallest-index-with-digit-sum-equal-to-index/smallest-index-with-digit-sum-equal-to-index.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int sum(int n)
+    {
+        int s=0;
+        while(n!=0)
+        {
+            int r=n%10;
+            s=s+r;
+            n=n/10;
+        }
+        return s;
+    }
+    int smallestIndex(vector<int>& nums) {
+        for(int i=0;i<nums.size();i++)
+        {
+            if(i==sum(nums[i]))
+                return i;
+        }
+        return -1;
+    }
+};
